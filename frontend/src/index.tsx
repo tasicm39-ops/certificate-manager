6ab03cf73c835dd9c6bfbfb0bc9b18bd945ab4be
Milos Tasic <tasicm39@gmail.com>
2026-03-25 22:18:12 +0100

    remove localhost baseURL

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import axios from 'axios'
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080'
 
-axios.defaults.baseURL = 'http://localhost:8080/api/v1/'
+axios.get(`${API_URL}/...`)
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
