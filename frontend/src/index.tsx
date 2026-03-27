69204c7049dae0fcc22a6cb5963459fdf53c7340
Milos Tasic <tasicm39@gmail.com>
2026-03-27 20:40:27 +0100

    fix unused variables

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import axios from 'axios'
-const API_URL = process.env.REACT_APP_API_URL || 'https://certificate-manager-2kda.onrender.com'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
